09421b857cf55af1c0a25185a2413bb902c83798
asif7897 <asifmahmud7894@gmail.com>
2024-11-04 08:19:25 +0600

    medusa config js for aws s3 file

--- medusa-config.ts
+++ medusa-config.ts
@@ -1,5 +1,5 @@
 import { loadEnv, defineConfig } from '@medusajs/framework/utils'
- 
+
 loadEnv(process.env.NODE_ENV || 'development', process.cwd())
 
 module.exports = defineConfig({
@@ -37,6 +37,26 @@ module.exports = defineConfig({
         redis: {
           url: process.env.REDIS_URL,
         },
+      },
+    },
+    {
+      resolve: "@medusajs/medusa/file",
+      options: {
+        providers: [
+          {
+            resolve: "@medusajs/medusa/file-s3",
+            id: "s3",
+            options: {
+              file_url: process.env.S3_FILE_URL,
+              access_key_id: process.env.S3_ACCESS_KEY_ID,
+              secret_access_key: process.env.S3_SECRET_ACCESS_KEY,
+              region: process.env.S3_REGION,
+              bucket: process.env.S3_BUCKET,
+              endpoint: process.env.S3_ENDPOINT,
+              // other options...
+            },
+          },
+        ],
       },
     },
   ]
